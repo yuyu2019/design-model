fd215e495ed55a2c72ded522d61424140abc06a7
yuyu2019 <53685248+yuyu2019@users.noreply.github.com>
2019-08-11 22:03:25 +0800

    Delete GPInvocationHandler.java

--- GPInvocationHandler.java
+++
@@ -1,9 +0,0 @@
-package com.gupaoedu.vip.pattern.proxy.dynamicproxy.gpproxy;
-
-import java.lang.reflect.Method;
-
-
-public interface GPInvocationHandler {
-    public Object invoke(Object proxy, Method method, Object[] args)
-            throws Throwable;
-}
